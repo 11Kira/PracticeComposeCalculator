4cdfd942774b303d0b89ff886640e01fb332490c
kira <kiravillagracia@yahoo.com>
2023-04-20 14:23:00 +0800

    created initial calculator functions in viewModel

--- app/src/main/java/com/kira/practicecalculatorcompose/CalculatorViewModel.kt
+++ app/src/main/java/com/kira/practicecalculatorcompose/CalculatorViewModel.kt
@@ -7,4 +7,36 @@ import androidx.lifecycle.ViewModel
 
 class CalculatorViewModel: ViewModel() {
     var state by mutableStateOf(CalculatorState())
+        private set
+
+    fun onAction(action: CalculatorAction) {
+        when (action) {
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Clear -> state = CalculatorState()
+            is CalculatorAction.Operation -> enterOperation(action.operations)
+            is CalculatorAction.Calculate -> performCalculation()
+            is CalculatorAction.Delete -> performDeletion()
+        }
+    }
+
+    private fun performDeletion() {
+        TODO("Not yet implemented")
+    }
+
+    private fun performCalculation() {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterOperation(operations: CalculatorOperations) {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterDecimal() {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterNumber(number: Int) {
+        TODO("Not yet implemented")
+    }
 }
