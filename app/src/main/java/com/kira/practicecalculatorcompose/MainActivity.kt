134bf5232782739ab4814e039dc75f4c933e8fd1
kira <kiravillagracia@yahoo.com>
2023-04-20 11:50:17 +0800

    cleanup of pre populated code

--- app/src/main/java/com/kira/practicecalculatorcompose/MainActivity.kt
+++ app/src/main/java/com/kira/practicecalculatorcompose/MainActivity.kt
@@ -3,13 +3,6 @@ package com.kira.practicecalculatorcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.kira.practicecalculatorcompose.ui.theme.PracticeCalculatorComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticeCalculatorComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PracticeCalculatorComposeTheme {
-        Greeting("Android")
     }
 }
