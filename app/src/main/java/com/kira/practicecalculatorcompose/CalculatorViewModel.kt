35f4fc28353e1a299eb813db1119481c394cb2b1
kira <kiravillagracia@yahoo.com>
2023-04-20 14:15:27 +0800

    Added calculator buttons

---
+++ app/src/main/java/com/kira/practicecalculatorcompose/CalculatorViewModel.kt
@@ -0,0 +1,10 @@
+package com.kira.practicecalculatorcompose
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+    var state by mutableStateOf(CalculatorState())
+}
--- app/src/main/java/com/kira/practicecalculatorcompose/MainActivity.kt
+++ app/src/main/java/com/kira/practicecalculatorcompose/MainActivity.kt
@@ -3,6 +3,8 @@ package com.kira.practicecalculatorcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kira.practicecalculatorcompose.ui.theme.PracticeCalculatorComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +12,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticeCalculatorComposeTheme {
-
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
             }
         }
     }
